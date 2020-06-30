11d874cb41eb94ff4ad26db8c6f13458967092e3
Lucas Song <lucas.song@LucasSong-MBPi.local>
2020-06-30 15:22:27 -0700

    Updates

---
+++ precache-manifest.58cf7567d2f21a4f339925b22c4cd40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f8a17b70ce1eeadad0ccdc8e30f0a8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4ed5f51b0677332b5e90",
+    "url": "/my-app/static/css/main.ff4e08c4.chunk.css"
+  },
+  {
+    "revision": "a87ea8de10cbf7548720",
+    "url": "/my-app/static/js/2.5603f76a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.5603f76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed5f51b0677332b5e90",
+    "url": "/my-app/static/js/main.3da6bdf0.chunk.js"
+  },
+  {
+    "revision": "eb977cc9a8134973c823",
+    "url": "/my-app/static/js/runtime-main.1cf9f878.js"
+  }
+]);
