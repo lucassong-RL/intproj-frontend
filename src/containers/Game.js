3b7699bde429d3d32720f9c35d9090d915d71d95
William Fuchs <william.fuchs@WillFuchs-MBPi.local>
2020-06-30 14:09:49 -0700

    split into columns

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -17,41 +17,45 @@ export default function Game(props) {
   }
   return (
     <GridWrapper>
-        <Col>
+        <Col style={{flexGrow: 1}}>
           <h1> {props.header} </h1>
           {props.description && <p> {props.description} </p>}
-          {props.gameId && 
-          <h5> you are in room 
-              <Button id="copy" onClick={() => copyToClipboard()}> <b>{props.gameId} </b> </Button> 
-          </h5>
-          }
-          {props.showQs && <div> this just in: <b>{props.newQs}</b> </div>}
-          {props.setQuestion && <FormControl size="lg" type="text" id="ask" placeholder="ask a question" onChange={e => props.setQuestion(e.target.value)}/>}
+          {props.showQs && <div> This just in: <b>{props.newQs}</b> </div>}
+          {props.setQuestion && <FormControl size="lg" type="text" id="ask" placeholder="Ask a question" onChange={e => props.setQuestion(e.target.value)}/>}
           {props.handleQuestionSubmit && <Button onClick={() => props.handleQuestionSubmit()}> submit </Button>}
-          {(props.admin===true) && <Button onClick={props.startRound}>start the answerin!</Button>}
-          {props.question && <h3> current question: {props.question} </h3>}
+          {(props.admin===true) && <Button onClick={props.startRound}>Start the answerin!</Button>}
+          {props.question && <h3> Current Question: {props.question} </h3>}
           {props.myTurn && 
               <>
-              <div> <b> you are answering! </b></div>
-              <Button id="finishans" onClick={props.finishQuestion}> finish answering </Button>
+              <div> <b> You are answering! </b></div>
+              <Button id="finishans" onClick={props.finishQuestion}> Finish Answering </Button>
               </>
           }
-          {(props.answerer && !props.myTurn) && <p> current answerer: {props.answerer} </p>}
+          {(props.answerer && !props.myTurn) && <p> Current Answerer: {props.answerer} </p>}
           {props.questions && <Questions questions={props.questions} handleQSelection={e => props.handleSelection(e)}/>}
           {props.potentialAns && props.potentialAns.map(data => <Button onClick={(e) => props.pickNextUser(e.target.innerText)}>{data}</Button>)}
         </Col>
-        <Col>
-            <Players players={props.players}/>
+        <Col style={{maxWidth: "30%"}}>
+            {/* <Players players={props.players}/> */}
+            <Players players={["some", "test", "player", "names", "and", "such"]}/>
+            {props.gameId && 
+            <h5> You are in room &nbsp;
+                <Button id="copy" onClick={() => copyToClipboard()}> <b>{props.gameId} </b> </Button> 
+            </h5>
+            }
         </Col>
     </GridWrapper>
   )
 }
 
 const GridWrapper = styled.div`
-    display: grid !important;
+    display: flex;
+    justify-content: space-between
 `
 
 const Col = styled.div`
-    grid-column: 1/2 !important;
+    min-width: 30%;
+    text-align: center;
+    padding: 1rem;
 `
 
