2bdf5c2177149d3ada1a764bc1065397225eb753
Lucas Song <lucas.song@LucasSong-MBPi.local>
2020-06-30 14:41:04 -0700

    add random generatorgst

--- src/components/Players.js
+++ src/components/Players.js
@@ -6,13 +6,19 @@ function randomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
+function randomColor2() {
+  let colors=["#ADD9EB", "#BDEFEA", "#EAD6A2", "#E7C4A6", "#E5B0A2"]
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+
 export default function Players(props) {
 
   const players = props.players
   return (
     <LobbyWrapper>
     
-      <HeaderWrapper style={{borderBottom: "1px solid #DADEDF", paddingBottom: "0.5rem"}}> <b> Players in Lobby </b> </HeaderWrapper>
+      <HeaderWrapper> <b> Players in Lobby </b> </HeaderWrapper>
       <PlayerWrapper>
       {players.length > 0 ? players.map(user => <Player style={{color: randomColor()}}> {user} </Player>) : <> None </>}
       </PlayerWrapper>
@@ -20,14 +26,15 @@ export default function Players(props) {
   );
 }
 
-const HeaderWrapper = styled.h4`
+const HeaderWrapper = styled.p`
+    font-size: 16px;
     color: ${randomColor}; 
     -webkit-text-stroke-width: 0.2px;
     -webkit-text-stroke-color: black;
 `
 
 const LobbyWrapper = styled.div`
-    background-color: ${randomColor}; 
+    background-color: ${randomColor2}; 
     opacity: 1;
     margin: 1rem 0rem;
     border-radius: 1rem;
